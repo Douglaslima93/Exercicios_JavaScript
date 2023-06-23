7c940249b653ab5a51fa635f8a0e2a192c5450a7
Douglas Lima <douglas07a@gmail.com>
2023-06-23 13:20:21 -0300

    Exercicios com While

---
+++ while/while2.js
@@ -0,0 +1,11 @@
+let numero = 5
+let contador = 10
+
+console.log("Tabuleiro de multiplicação N°" + numero)
+
+while (contador <=20)
+{
+    let resultado = numero * contador
+    console.log(numero + "x" + contador + "=" + resultado)
+    contador++
+}
