96cdbdd868c99090001456d20f363a024429d831
Douglas Lima <douglas07a@gmail.com>
2023-06-27 13:12:51 -0300

    Funções

---
+++ funcoes/status.aluno.js
@@ -0,0 +1,12 @@
+function retorna_status_aluno(nota_1, nota_2) {
+    let media = (nota_1 + nota_2) /2
+    let status
+
+    if (media >=6){
+        status="aprovado"
+    } else {
+        status= "reprovado"
+    }
+
+    return status
+}
---
+++ funcoes/substituindo.substring.js
@@ -0,0 +1,4 @@
+let frase = "Estou manipulando strings na linguagem PHP"
+let nova_frase = frase.replace("PHP", "JavaScript")
+
+console.log(nova_frase)
