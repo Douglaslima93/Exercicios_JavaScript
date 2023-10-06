598ac9df5e4110e6ab86f6517a9ff1dae721b966
Douglas Lima <121909515+Douglaslima93@users.noreply.github.com>
2023-10-06 15:32:10 -0300

    Update arrow.function1.js

--- funcoes/arrow.function1.js
+++ funcoes/arrow.function1.js
@@ -1,9 +1,9 @@
-const retorna_media = (nota_1, nota_2) => {
+const retorna_media = (nota_1, nota_2, nota_3) => {
 
-    let media = (nota_1 + nota_2) / 2;
+    let media = (nota_1 + nota_2 + nota_3) / 2;
     return media;
 
 }
 
-let media_aluno = retorna_media(9, 8);
+let media_aluno = retorna_media(9, 8, 7);
 console.log("A média do aluno é: " + media_aluno);
