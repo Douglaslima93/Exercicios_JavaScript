00b7ea1e4118ef0cf9fae4c327873432acccc9a5
Douglas Lima <121909515+Douglaslima93@users.noreply.github.com>
2023-10-07 18:53:43 -0300

    Atualizar o for_continue.js

--- for/for_continue.js
+++ for/for_continue.js
@@ -4,6 +4,7 @@ let alunos = [
     {id: 3, nome: "José", media: 5},
     {id: 4, nome: "Guilherme", media: 4},
     {id: 5, nome: "Rafael", media: 10},
+    {id: 6, nome: "Laís", media: 6},
    ];
  
    for ( let contador = 0; contador < alunos.length; contador++ ) {
